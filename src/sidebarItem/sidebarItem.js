f2cc31dc7a87a50555b6896b01c957d6cb3ef50c
Benji Mirschel <cliff.mirschel@gmail.com>
2019-07-16 10:11:46 -0400

    Pushing Finished app to git

--- src/sidebarItem/sidebarItem.js
+++ src/sidebarItem/sidebarItem.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import styles from './styles';
@@ -6,15 +7,38 @@ import ListItemText from '@material-ui/core/ListItemText';
 import DeleteIcon from '@material-ui/icons/Delete';
 import { removeHTMLTags } from '../helpers';
 
-class SidebarItemComponent extends React.Component{
-    constructor(){
-        super();
+class SidebarItemComponent extends React.Component {
 
-    }
+  render() {
 
-    render(){
-        return (<div>Hello from the sidebar item</div>);
+    const { _index, _note, classes, selectedNoteIndex } = this.props;
+
+    return(
+      <div key={_index}>
+        <ListItem
+          className={classes.listItem}
+          selected={selectedNoteIndex === _index}
+          alignItems='flex-start'>
+            <div 
+              className={classes.textSection}
+              onClick={() => this.selectNote(_note, _index)}>
+                <ListItemText
+                  primary={_note.title}
+                  secondary={removeHTMLTags(_note.body.substring(0, 30)) + '...'}></ListItemText>
+            </div>
+            <DeleteIcon onClick={() => this.deleteNote(_note)}
+              className={classes.deleteIcon}></DeleteIcon>
+        </ListItem>
+      </div>
+    );
+  }
+  selectNote = (n, i) => this.props.selectNote(n, i);
+  deleteNote = (note) => {
+    if(window.confirm(`Are you sure you want to delete: ${note.title}`)) {
+      this.props.deleteNote(note);
     }
+  }
+
 }
 
 export default withStyles(styles)(SidebarItemComponent);
