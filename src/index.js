361501ab51a2867bff62a09ef25ff852c8a83427
Benji Mirschel <cliff.mirschel@gmail.com>
2019-07-16 12:43:54 -0400

    Finished basic signup page and routing

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {Route, BrowserRouter as Router} from 'react-router-dom';
+import LoginComponent from './login/login';
+import SignupComponent from './signup/signup';
 
 const firebase = require('firebase');
 require('firebase/firestore');
@@ -19,7 +22,17 @@ const firebaseConfig = {
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
-ReactDOM.render(<App />, document.getElementById('notify-container'));
+const routing =(
+  <Router>
+    <div id='routing-container'>
+      <Route exact path='/login' component={LoginComponent}></Route>
+      <Route  exact path='/signup' component={SignupComponent}></Route>
+      <Route path='/dashboard' component={App}></Route> 
+    </div>
+  </Router>
+)
+
+ReactDOM.render(routing, document.getElementById('notify-container'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/login/login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class LoginComponent extends React.Component{
+
+
+    render(){
+        return (
+        <div>
+        Hello world from login
+        </div>)
+    }
+
+}
+
+export default LoginComponent;
